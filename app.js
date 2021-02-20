b44da626a810f0452740e1c33540401cbfe6f780
SRIRAM238 <75298072+SRIRAM238@users.noreply.github.com>
2021-02-20 19:52:20 +0530

    modified README.md,added images and other changes

--- app.js
+++ app.js
@@ -1,44 +1,30 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const app = express();
-// app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
-app.get("/tshirts", function(req,res) {
+
+app.get("/tshirts", function(req, res) {
   res.status(200).send({
-    tshirt:"👕",
-    size:"large"
+    tshirt: "👕",
+    size: "large"
   });
 });
 
-app.post("/tshirts/:id", function(req,res) {
-const {id} = req.params;
-const {logo} = req.body;
-
-if(!logo)
-{
-  res.send({message:"We need the logo"});
-}else{
-  res.send({
-    tshirt:"👕 that contains"+ logo + "and id of" + id
-  });
-}
-
+app.post("/tshirts/:id", function(req, res) {
+  const {id} = req.params;
+  const {logo} = req.body;
+  if (!logo) {
+    res.send({
+      message: "We need the logo"
+    });
+  } else {
+    res.send({
+      tshirt: "👕 that contains" + logo + "and id of" + id
+    });
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen("3000", function() {
 
-console.log("server is running on port number 3000");
+  console.log("server is running on port number 3000");
 
 });
