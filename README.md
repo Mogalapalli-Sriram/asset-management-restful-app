30812521fb69ef605e1a59dcb82184f77f11bba3
SRIRAM238 <75298072+SRIRAM238@users.noreply.github.com>
2021-02-21 22:52:42 +0530

    added all api's,used mongoose ,mongodb to persist the data and populated readme.md with all apis

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ The code is about using RESTful API's from scratch by which we can render the da
 * code text editors (Atom or visual studio code)
 * Express.js
 * Node.js
-* Hyper Terminal
+* Hyper Terminal(CLI) or Robo3T(GUI)
+* MongoDB
+* Mongoose
 * API clients(postman or insomnia)
 
 
@@ -25,25 +27,56 @@ The code is about using RESTful API's from scratch by which we can render the da
 
 ![install npm](images/installnpms.PNG)
 
-2. Start the server 
+2. Start the server.
 
 ![starting the server](images/serverstart.PNG)
 
 3. You can access the app at http://localhost:3000
 
-4. Use the API client [insomnia](https://insomnia.rest/) to generate the HTTP request verbs.
+4. Download,Install MongoDB and Start the Mongo server(It is hosted on port 27017).
+
+
+5. Use the API client [postman](https://insomnia.rest/) to generate the HTTP request verbs.
 
 
 
 ## API'S I have used in my App?
 
-I have made use of the Post and Get request verbs of HTTP. Used Post to write(insert) the data to the server where the data can be reused in the server.
+I have made use of the all the request verbs of HTTP(API's) to perform the CRUD operations for the main route and specific route .The list of all API's I have used are shown below.
 
+### Get All Tshirt
 
+![curlgetall](images/getallcurl.PNG)
+![getallresult](images/getallresult.PNG)
+
+### Create Tshirt
+
+![curlpost](images/curlpost.PNG)
+![postresult](images/resultpost.PNG)
+
+### Get Specific Tshirt
+
+![getcurlone](images/getspecificcurl.PNG)
+![getresultone](images/getspecificresult.PNG)
+
+### Update Entire Tshirt
+
+![putcurl](images/putrequestcurl.PNG)
+![putresult](images/putrequestresult.PNG)
+
+### Update part of Tshirt Document
+
+![patchcurl](images/patchrequestcurl.PNG)
+![patchresult](images/patchrequestresult.PNG)
+
+### Remove Tshirt
+
+![deletecurl](images/deleterequestcurl.PNG)
+![deleteresult](images/deleterequestresponse.PNG)
 
 ## Accessing the API's
 
-I have accessed the API's such as Get and Post by using the express application and used route and callback function in Get and Post function call.
+I have accessed the API's such as Get,Post,Put,Patch,Delete by using the express application and used route and callback function in all API function calls.
 
 
 
--- app.js
+++ app.js
@@ -1,27 +1,131 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+mongoose.connect("mongodb://localhost:27017/tshirtsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const shirtsSchema = new mongoose.Schema({
+  tshirt: String,
+  size: String
+});
+const Tshirt = mongoose.model("Tshirt",shirtsSchema);
 
+/***************************HTTP request verbs for the main route******************************************/
 app.get("/tshirts", function(req, res) {
-  res.status(200).send({
-    tshirt: "👕",
-    size: "large"
+  Tshirt.find(function(err, findItems) {
+    if (findItems) {
+      res.send({
+        "success": "true",
+        "data": findItems
+      });
+    } else {
+      res.send("error");
+    }
   });
 });
 
-app.post("/tshirts/:id", function(req, res) {
-  const {id} = req.params;
-  const {logo} = req.body;
-  if (!logo) {
-    res.send({
-      message: "We need the logo"
-    });
-  } else {
-    res.send({
-      tshirt: "👕 that contains" + logo + "and id of" + id
-    });
-  }
+
+app.post("/tshirts", function(req, res) {
+  const newTshirt = new Tshirt({
+    tshirt: req.body.tshirt,
+    size: req.body.size
+  });
+  newTshirt.save(function(err) {
+    if (err) {
+      res.send("error");
+    } else {
+      res.send({
+        "success": "true",
+        "data": {
+          "tshirt": req.body.tshirt,
+          "size": req.body.size
+        }
+      });
+    }
+  });
 });
+
+/************************************HTTP request verbs for a specific route*********************************************/
+app.get("/tshirts/:shirtModel", function(req, res) {
+  const shirtName = req.params.shirtModel;
+  Tshirt.findOne({
+    tshirt: shirtName
+  }, function(err, findItem) {
+    if (findItem) {
+      res.send({
+        "success": "true",
+        "data": findItem
+      });
+    } else {
+      res.send("error");
+    }
+  });
+
+});
+
+
+
+app.put("/tshirts/:shirtModel", function(req, res) {
+  Tshirt.update({
+      tshirt: req.params.shirtModel
+    }, {
+      tshirt: req.body.tshirt,
+      size: req.body.size
+    }, {
+      overwrite: true
+    },
+    function(err) {
+      if (!err) {
+        res.send("Successfully updated the entire document");
+      } else {
+        res.send("error");
+      }
+
+    });
+});
+
+
+app.patch("/tshirts/:shirtModel", function(req, res) {
+  Tshirt.update({
+      tshirt: req.params.shirtModel
+    }, {
+      $set: req.body
+    },
+    function(err) {
+      if (!err) {
+        res.send( {
+          "success":"true",
+          "message":"updated the part of document"
+        });
+      } else {
+        res.send("error");
+      }
+    }
+  );
+});
+
+
+app.delete("/tshirts/:shirtModel", function(req, res) {
+  Tshirt.deleteOne({
+      tshirt: req.params.shirtModel
+    },
+    function(err) {
+      if (!err) {
+        res.send("removed the item");
+      } else {
+        res.send("error");
+      }
+    }
+  );
+
+});
+
 
 app.listen("3000", function() {
 
